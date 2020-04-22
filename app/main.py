8c4d66d74373d0b85037b392056d7479042ad525
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-04-22 21:02:04 +0300

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,14 @@
+#!../venv/bin/python
+# -*- coding: UTF-8 -*-
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "<h1>Home Page</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
