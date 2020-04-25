860985d6aa56d34ebb1dbe0059fe73050a782556
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-04-25 21:00:21 +0300

    Added <view> for register

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 #!../venv/bin/python
 # -*- coding: UTF-8 -*-
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import Registration, Login
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '196e40bf26441892bd1b5ffc1093acfec6d55b73'
 
 posts = [
     {
@@ -31,6 +34,21 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = Registration()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = Login()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
