d0c6527d61b8d10028ac3f2a8bd288aa8b41e3a0
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-05-08 21:38:41 +0300

    Implemented basic profile info

--- app/views.py
+++ app/views.py
@@ -78,4 +78,5 @@ def logout():
 @app.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html', title='Profile')
+    image_file = url_for('static', filename=f'profile_pics/{current_user.image_file}')
+    return render_template('profile.html', title='Profile', image_file=image_file)
