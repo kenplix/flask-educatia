330b47dce03413e2339b61cf12f170205068c434
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-05-17 15:41:52 +0300

    Added sorting by newest post

--- app/views.py
+++ app/views.py
@@ -14,7 +14,7 @@ from app.models import User, Post
 @app.route('/home')
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.paginate(page=page, per_page=2)
+    posts = Post.query.order_by(Post.date.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 
