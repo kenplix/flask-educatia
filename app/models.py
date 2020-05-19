7629d896d2faf97b77c6146e4782d4d202f546a6
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-05-19 22:45:43 +0300

    Added JWS for resetting password

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from typing import Optional
 
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
-from app import db, login_manager
+from app import db, login_manager, app
 
 
 @login_manager.user_loader
@@ -16,8 +18,20 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-
     posts = db.relationship('Post', backref='author', lazy=True)
+
+    def reset_token(self, expires_sec: int = 1800) -> str:
+        ser = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return ser.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify(reset_token: str) -> Optional['User']:
+        ser = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = ser.loads(reset_token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f'User <{self.username}: {self.email}>'
@@ -28,7 +42,6 @@ class Post(db.Model):
     title = db.Column(db.String(100), nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
