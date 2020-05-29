2d07579a435895fcdf818fa0a1b25cab849b0972
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-05-29 21:02:40 +0300

    Implemented migration system

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
@@ -7,6 +8,7 @@ from flask_mail import Mail
 from app.config import Config
 
 db = SQLAlchemy()
+migrate = Migrate()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_message_category = 'info'
@@ -19,6 +21,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
+    migrate.init_app(app, db)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
