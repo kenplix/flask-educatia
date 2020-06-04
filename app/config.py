9364a67ef8b0933e8f4bb3ebba93aae0036fb946
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-06-04 17:48:31 +0300

    Little config changes

--- app/config.py
+++ app/config.py
@@ -2,15 +2,24 @@ import os
 
 from dotenv import load_dotenv
 
-# load dotenv in the base root
+# Load dotenv in the base root
 APP_ROOT = os.path.join(os.path.dirname(__file__), '..')
 DOTENV_PATH = os.path.join(APP_ROOT, '.env')
 load_dotenv(DOTENV_PATH)
 
 
 class Config:
+    """Set Flask configuration variables from .env file."""
+
+    # General Flask Config
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    DEBUG = True
+
+    # Database
     SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Mail
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
