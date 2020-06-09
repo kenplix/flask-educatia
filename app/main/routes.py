9782b64a34e0d07c6823425fc0b8b575006a9ad6
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-06-09 21:14:07 +0300

    Implemented search system

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,13 +5,18 @@ from app.models import Post
 main = Blueprint('main', __name__)
 
 
+def paginate(page, posts, per_page=5):
+    return posts.order_by(Post.date.desc()).paginate(page=page, per_page=per_page)
+
+
 @main.route('/')
 @main.route('/home')
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date.desc())\
-        .paginate(page=page, per_page=5)
-    return render_template('main/home.html', posts=posts)
+    posts = Post.query
+    if query := request.args.get('query'):
+        posts = posts.filter(Post.title.contains(query) | Post.content.contains(query))
+    return render_template('main/home.html', posts=paginate(page, posts))
 
 
 @main.route('/about')
