3831d2b1e774ed2c1b7bde0c9624e452cd7fd4f7
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-06-09 22:23:56 +0300

    Added admin panel

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 from app.config import Config
 
@@ -14,6 +16,7 @@ login_manager = LoginManager()
 login_manager.login_message_category = 'info'
 login_manager.login_view = 'users.login'
 mail = Mail()
+admin = Admin()
 
 
 def create_app(config_class=Config):
@@ -25,10 +28,15 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    admin.init_app(app)
 
     @app.cli.command()
     def createdb():
         db.create_all()
+
+    from app.models import Post, Tag
+    admin.add_view(ModelView(Post, db.session))
+    admin.add_view(ModelView(Tag, db.session))
 
     from app.main.routes import main
     from app.users.routes import users
