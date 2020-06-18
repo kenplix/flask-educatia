bc26203e732b59389e9ea6dfc2b4b272f86f60e6
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-06-18 23:51:07 +0300

    Added <followed_posts> method

--- app/models.py
+++ app/models.py
@@ -91,6 +91,13 @@ class User(UserMixin, db.Model):
         return self.followed.filter(
             Followers.c.followed_id == user.id).count() > 0
 
+    def followed_posts(self):
+        followed = Post.query.join(
+            Followers, (Followers.c.followed_id == Post.user_id)).filter(
+                Followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+
     def __repr__(self):
         return f'User #{self.id} <{self.username}: {self.email}>'
 
